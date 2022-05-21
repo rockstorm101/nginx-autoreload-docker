92c07e1f320aad9311ef43d7a2fa466d4b8b0776
Rock Storm <rockstorm@gmx.com>
2022-05-21 08:53:54 +0100

    Allow word-splitting for multiple files

--- autoreload.sh
+++ autoreload.sh
@@ -3,8 +3,7 @@
 set -e
 
 _autoreload() {
-    while inotifywait --event create,modify,delete --quiet \
-                      "${AUTORELOAD_FILES}" 
+    while inotifywait --event create,modify,delete --quiet ${AUTORELOAD_FILES}
     do
         nginx -s reload
     done
